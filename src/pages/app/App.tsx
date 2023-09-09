4aaa6fd87213b36d4b6d56c3de04c629d7020920
srilan <devd@Devs-MacBook-Pro.local>
2023-09-09 18:04:30 +0800

    moved them to organize structure

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { NavigationBar } from '../../components/NavigationBar';
 
 function App() {
   return (
+    <>
+    <NavigationBar />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
