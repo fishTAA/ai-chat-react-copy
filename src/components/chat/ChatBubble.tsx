538ba63c7b112c345b18826dec740d09cf947861
srilan <devd@Devs-MacBook-Pro.local>
2023-09-10 18:23:30 +0800

    chat layout

--- src/components/chat/ChatBubble.tsx
+++ src/components/chat/ChatBubble.tsx
@@ -1,9 +1,9 @@
 import { Box, Columns, Heading } from "react-bulma-components";
 
 interface ChatBubbleParams {
-  sender: String;
-  timeSent: String;
-  message: String;
+  sender?: String;
+  timeSent?: String;
+  message?: String;
   position?: "left" | "right";
 }
 
@@ -15,29 +15,45 @@ export const ChatBubble = (params: ChatBubbleParams) => {
         paddingLeft: 20,
         paddingRight: 20,
         marginBottom: 20,
+        marginLeft: params.sender === '_self' ? 20 : 0,
+        marginRight: params.sender === '_self' ? 0 : 20,
+        display: "flex",
+        flexDirection: 'column',
+        alignItems: params.sender === '_self' ? "end" : "start",
       }}
     >
       <Box
         shadowless
         style={{
           margin: 0,
-          padding: 0
+          padding: 0,
+          fontSize: 12,
+          fontWeight: "bold",
+          textAlign: params.sender === '_self' ? "right": "left",
         }}
       >
-        {params.sender}
+        {params.sender === '_self' ? 'Me' : params.sender}
       </Box>
       <Box
         style={{
           margin: 0,
           paddingTop: 10,
           paddingBottom: 10,
+          backgroundColor: "#6ab1fc",
+          display: 'flex',
+          inlineSize: 'fit-content',
+          flexDirection: 'column',
         }}
       >
         <Box
           shadowless
           style={{
             margin: 0,
-            padding: 0
+            padding: 0,
+            background: "none",
+            color: "white",
+            fontSize: 14,
+            overflowWrap: "anywhere",
           }}
         >
           {params.message}
@@ -47,13 +63,14 @@ export const ChatBubble = (params: ChatBubbleParams) => {
           style={{
             margin: 0,
             padding: 0,
-            display: "flex"
+            display: "flex",
+            background: "none",
           }}
         >
           <span style={{
             marginLeft: "auto",
             color: "GrayText",
-            fontSize: 12,
+            fontSize: 8,
           }}>
             {params.timeSent}
           </span>
