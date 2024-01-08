49d6ce8a3f316640f70162b3924c9a051a7f61b5
Tom Anthony Aguila <tom31aguila@gmail.com>
2024-01-08 14:00:55 -0800

    [add-in progress] send token to BE

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,17 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import { NavigationBar } from '../../components/NavigationBar';
-import { Card, Columns, Container, Content, Footer, Heading, Hero, Media, Form, Table,Button,Block, Section } from 'react-bulma-components';
-import Chat from '../../components/chat';
-import { Ticket } from '../../components/Ticket';
-import { FooterSection } from '../../components/Footer';
-import Manage from '../manage';
-import { useLocation, useNavigate, useParams,Link} from "react-router-dom";
-import { BeatLoader } from 'react-spinners';
-import image from '../../media/image.png';
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsalAuthentication, useIsAuthenticated, useMsal, useAccount } from "@azure/msal-react";
-import { InteractionStatus, InteractionType } from '@azure/msal-browser';
-
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import { NavigationBar } from "../../components/NavigationBar";
+import {
+  Card,
+  Columns,
+  Container,
+  Content,
+  Footer,
+  Heading,
+  Hero,
+  Media,
+  Form,
+  Table,
+  Button,
+  Block,
+  Section,
+} from "react-bulma-components";
+import Chat from "../../components/chat";
+import { Ticket } from "../../components/Ticket";
+import { FooterSection } from "../../components/Footer";
+import Manage from "../manage";
+import { useLocation, useNavigate, useParams, Link } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
+import image from "../../media/image.png";
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsalAuthentication,
+  useIsAuthenticated,
+  useMsal,
+  useAccount,
+} from "@azure/msal-react";
+import { InteractionStatus, InteractionType } from "@azure/msal-browser";
+import { handleSendTokenToBackend } from "../../components/dbFunctions/sendTokentoBE";
 
 function App() {
   // Check if the user is authenticated
@@ -19,17 +40,17 @@ function App() {
 
   // Define the TestInterface for clarity
   interface TestInterface {
-    _id: string,
-    input: string,
-    title: string,
-    score: number,
-    solution?: string
+    _id: string;
+    input: string;
+    title: string;
+    score: number;
+    solution?: string;
   }
 
   let navigate = useNavigate();
 
   // Define the API endpoint, defaulting to 'http://localhost:8000' if not provided
-  const endPoint = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+  const endPoint = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
   // State variables for managing loading, form visibility, test results, and document
   const [loadingTest, setLoadingTest] = useState(false);
@@ -43,259 +64,260 @@ function App() {
 
   // Get user account based on the stored account data
   const userAccount = useAccount(JSON.parse(account));
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     // Check if the authentication process is not in progress and the user is not authenticated
     if (inProgress === InteractionStatus.None && !isAuthenticated) {
-      setLoadingTest(true)
+      setLoadingTest(true);
 
-      if(account) {
+      if (account) {
         // Attempt to acquire a silent token for the user
-        instance.acquireTokenSilent({
-          account: JSON.parse(account),
-          scopes: ["openid", "profile"],
-        }).then(e => {
-          setLoadingTest(false);
-        }).catch(e=> {
-          console.log("here", e)
 
-          // Redirect to the login page on token acquisition failure
-          navigate('/login')
-        });
+        const token = instance
+          .acquireTokenSilent({
+            account: JSON.parse(account),
+            scopes: ["openid", "profile"],
+          })
+          .then((e) => {
+            setLoadingTest(false);
+          })
+          .catch((e) => {
+            console.log("here", e);
+
+            // Redirect to the login page on token acquisition failure
+            navigate("/login");
+          });
       } else {
         // Redirect to the login page if no account data is available
-        navigate('/login')
+        navigate("/login");
       }
     }
   }, [inProgress]);
 
   const handleTestEmbeddings = () => {
-      // Set loading state to indicate an operation is in progress
-      setLoadingTest(true);
+    // Set loading state to indicate an operation is in progress
+    setLoadingTest(true);
 
-      // Perform a POST request to the 'testEmbedding' endpoint using the fetch API
-      fetch(`${endPoint}/testEmbedding`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          keyword: document,
-        })
-      }).then((res)=> {
+    // Perform a POST request to the 'testEmbedding' endpoint using the fetch API
+    fetch(`${endPoint}/testEmbedding`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        keyword: document,
+      }),
+    })
+      .then((res) => {
         // Handle the response from the server
-        return res.json()
-      }).then((res)=> {
-
+        return res.json();
+      })
+      .then((res) => {
         // Set the test results and log the related data
         setTestResults(res.related);
         console.log(res.related);
-      }).finally(()=> {
-        setLoadingTest(false);
       })
-    }
+      .finally(() => {
+        setLoadingTest(false);
+      });
+  };
 
   return (
     <>
-      { showTicketForm && (
+      {showTicketForm && (
         // Render the Ticket component when showTicketForm is true
-        < Ticket  setShowTicketForm={setShowTicketForm} />
+        <Ticket setShowTicketForm={setShowTicketForm} />
       )}
       <div
         style={{
-        backgroundImage: `url(${image})`,
-        height: 'auto',
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed',
-        margin: 0
-      }}>
-      <NavigationBar/>
-      <Hero 
-        size="fullheight"  
+          backgroundImage: `url(${image})`,
+          height: "auto",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          margin: 0,
+        }}
       >
-        <Hero.Body
-          style={{
-          paddingTop: 100,
-        }}>
-          <Container> 
-          <Form.Field kind="addons">
-            <Form.Control fullwidth>
-              <Form.Input
-                    onChange={(e)=>setDocument(e.target.value)}
-                    onKeyDown={(e)=>{
+        <NavigationBar />
+        <Hero size="fullheight">
+          <Hero.Body
+            style={{
+              paddingTop: 100,
+            }}
+          >
+            <Container>
+              <Form.Field kind="addons">
+                <Form.Control fullwidth>
+                  <Form.Input
+                    onChange={(e) => setDocument(e.target.value)}
+                    onKeyDown={(e) => {
                       if (e.key === `Enter`)
-                        
-                      // Execute handleTestEmbeddings on Enter key press
-                        handleTestEmbeddings()
-                      }}
-                      
+                        // Execute handleTestEmbeddings on Enter key press
+                        handleTestEmbeddings();
+                    }}
                     value={document}
-                    placeholder={"Search "}                      
-                      style={{
-                      boxShadow: '2px 2px 5px 0px #888888',
+                    placeholder={"Search "}
+                    style={{
+                      boxShadow: "2px 2px 5px 0px #888888",
                       borderTopRightRadius: 0,
                       borderBottomRightRadius: 0,
                       borderTopLeftRadius: 20,
                       borderBottomLeftRadius: 20,
-                      borderRight: 'none',
+                      borderRight: "none",
                       maxHeight: "50px",
                       minHeight: "50px",
                       overflow: "hidden",
-                      resize:"none",
+                      resize: "none",
                       marginTop: -50,
                     }}
-                    />
-            </Form.Control>
-            <Form.Control>
-              <Button
-              onClick={()=>{
-                // Clear the document and test results
-                setDocument('');
-                setTestResults([]);
-              }}
-              style={{
-                boxShadow: '2px 2px 5px 0px #888888',
-                borderTopRightRadius: 20,
-                borderBottomRightRadius: 20,
-                borderTopLeftRadius: 0,
-                borderBottomLeftRadius: 0,
-                borderLeft: 'none',
-                maxHeight: "50px",
-                minHeight: "50px",
-                overflow: "hidden",
-                resize:"none",
-                marginTop: -50,
-              }}
+                  />
+                </Form.Control>
+                <Form.Control>
+                  <Button
+                    onClick={() => {
+                      // Clear the document and test results
+                      setDocument("");
+                      setTestResults([]);
+                    }}
+                    style={{
+                      boxShadow: "2px 2px 5px 0px #888888",
+                      borderTopRightRadius: 20,
+                      borderBottomRightRadius: 20,
+                      borderTopLeftRadius: 0,
+                      borderBottomLeftRadius: 0,
+                      borderLeft: "none",
+                      maxHeight: "50px",
+                      minHeight: "50px",
+                      overflow: "hidden",
+                      resize: "none",
+                      marginTop: -50,
+                    }}
+                  >
+                    Clear
+                  </Button>
+                </Form.Control>
+              </Form.Field>
+
+              <Form.Field
+                style={{
+                  paddingInline: 100,
+                }}
               >
-                Clear
-                </Button>
-            </Form.Control>
-          </Form.Field>
-
-          <Form.Field
-          style={{
-            paddingInline: 100,
-          }}
-            >
-            {loadingTest? (
-            // Display a loading spinner when loadingTest is true
-            <>
-              <Block
-              style={{
-                display: 'flex' ,
-                flexDirection: 'column',
-                alignItems: 'center', 
-              }}>
-                <BeatLoader
-                  color="#36d7b7"
-                  size={35} />
-              </Block>
-          </>
-          ):null}
-          </Form.Field>
-
-          <section>
-            <Columns
-            style={{
-              paddingTop: 20
-            }}>
-              {/* Map through the test results and render each one */}
-              {testResults.map((res)=>{        
-                    return (
-                      <Columns.Column className='is-one-third'
+                {loadingTest ? (
+                  // Display a loading spinner when loadingTest is true
+                  <>
+                    <Block
                       style={{
-                        display: 'flex' ,
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                      }}>
-                      <Card
-                      style={{
-                        maxWidth: '70%',
-                        minWidth: '100%',
-                        margin: 10,
-                        minHeight: '100%'
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
                       }}
-                        onClick={()=> {
-                          // Navigate to view solution page with relevant information
-                          navigate('view-solution/'+res._id+'/'+document)
-                        }}>
-                            <Card.Content>
-                              <Media>
-                                <Media.Item>
-                                  <Heading
-                                  
-                                  size={4}
-                                    >
-                                      {res.title}
-                                    </Heading>
-                                
-                                </Media.Item>
-                              </Media>
-                              <Content>
-                                {res.input}
-                              </Content>
-                            </Card.Content>
-                        </Card>
+                    >
+                      <BeatLoader color="#36d7b7" size={35} />
+                    </Block>
+                  </>
+                ) : null}
+              </Form.Field>
 
-                        
-                      </Columns.Column> 
-                    )
-              })}   
-              
-              <Columns.Column className='is-one-third'
-                      style={{
-                        display: 'flex' ,
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                      }}>
-                <Card 
-                  onClick={()=>{
-                    // Render a card for submitting a ticket
-                    setShowTicketForm(true)}}
+              <section>
+                <Columns
                   style={{
-                    width: '100%',
-                    margin: 10,
-                    minHeight: '100%',
-                    backgroundColor: '#307FE2',
-                    cursor: 'pointer'
-                    }}>
-                  <Card.Content>
-                    <Media>
-                      <Media.Item>
-                        <Heading size={4}
+                    paddingTop: 20,
+                  }}
+                >
+                  {/* Map through the test results and render each one */}
+                  {testResults.map((res) => {
+                    return (
+                      <Columns.Column
+                        className="is-one-third"
                         style={{
-                          color:"white",
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Card
+                          style={{
+                            maxWidth: "70%",
+                            minWidth: "100%",
+                            margin: 10,
+                            minHeight: "100%",
                           }}
-                          >
-                            Submit Ticket
-                          </Heading>
-                      </Media.Item>
-                    </Media>
-                    <Content style={{
-                          color:"white",
-                          }}>
-                      Can't find what you're looking for? Submit a ticket!
-                    </Content>
-                  </Card.Content>
-              </Card>    
-            </Columns.Column>
-            </Columns>
-            </section>
-          </Container>
-        </Hero.Body>
-      <Chat 
-        width={350}
-      />
-      <Hero.Footer
-      marginless paddingless
-      >
-    <FooterSection />
-  </Hero.Footer>
-  </Hero>
-</div>
-     </>
+                          onClick={() => {
+                            // Navigate to view solution page with relevant information
+                            navigate(
+                              "view-solution/" + res._id + "/" + document
+                            );
+                          }}
+                        >
+                          <Card.Content>
+                            <Media>
+                              <Media.Item>
+                                <Heading size={4}>{res.title}</Heading>
+                              </Media.Item>
+                            </Media>
+                            <Content>{res.input}</Content>
+                          </Card.Content>
+                        </Card>
+                      </Columns.Column>
+                    );
+                  })}
+
+                  <Columns.Column
+                    className="is-one-third"
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Card
+                      onClick={() => {
+                        // Render a card for submitting a ticket
+                        setShowTicketForm(true);
+                      }}
+                      style={{
+                        width: "100%",
+                        margin: 10,
+                        minHeight: "100%",
+                        backgroundColor: "#307FE2",
+                        cursor: "pointer",
+                      }}
+                    >
+                      <Card.Content>
+                        <Media>
+                          <Media.Item>
+                            <Heading
+                              size={4}
+                              style={{
+                                color: "white",
+                              }}
+                            >
+                              Submit Ticket
+                            </Heading>
+                          </Media.Item>
+                        </Media>
+                        <Content
+                          style={{
+                            color: "white",
+                          }}
+                        >
+                          Can't find what you're looking for? Submit a ticket!
+                        </Content>
+                      </Card.Content>
+                    </Card>
+                  </Columns.Column>
+                </Columns>
+              </section>
+            </Container>
+          </Hero.Body>
+          <Chat width={350} />
+          <Hero.Footer marginless paddingless>
+            <FooterSection />
+          </Hero.Footer>
+        </Hero>
+      </div>
+    </>
   );
 }
 
 export default App;
-
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Button, Box, Hero, Heading } from 'react-bulma-components';
-import { MsalProvider, useMsal, useAccount } from '@azure/msal-react';
-import { loginRequest,logoutRequest,pca } from '../../authconfig';
-import { PublicClientApplication, EventType, EventMessage, AuthenticationResult } from "@azure/msal-browser";
-import { useNavigate } from 'react-router-dom';
-import logo from '../../media/Trajector Main Logo_Color.png';
-import background from '../../media/MountainWavesBlue.png';
+import React from "react";
+import { Button, Box, Hero, Heading } from "react-bulma-components";
+import { MsalProvider, useMsal, useAccount } from "@azure/msal-react";
+import { loginRequest, logoutRequest, pca } from "../../authconfig";
+import {
+  PublicClientApplication,
+  EventType,
+  EventMessage,
+  AuthenticationResult,
+} from "@azure/msal-browser";
+import { useNavigate } from "react-router-dom";
+import logo from "../../media/Trajector Main Logo_Color.png";
+import background from "../../media/MountainWavesBlue.png";
+import { handleSendTokenToBackend } from "../../components/dbFunctions/sendTokentoBE";
 
 function Login() {
-
   // Get MSAL instance and accounts
   const { instance, accounts } = useMsal();
 
@@ -24,81 +29,90 @@ function Login() {
         //  console.log(accounts)
         //}));
         await instance.loginPopup().then(async (res) => {
-          console.log("login", res)
-
+          console.log("login", res);
+          const Token = res.accessToken;
+    
+          
           // Store the authenticated account in local storage
           localStorage.setItem("account", JSON.stringify(res.account));
           // Navigate to the main application page
+          handleSendTokenToBackend(Token);
           navigate("/");
         });
       } else {
         // User is already authenticated
-        console.log('User is already logged in.');
+        console.log("User is already logged in.");
       }
     } catch (error) {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
     }
     return false;
   };
 
   // Function to handle the logout process
   const handleLogout = () => {
-    instance.logoutRedirect(logoutRequest)
+    instance.logoutRedirect(logoutRequest);
   };
 
   return (
-      <div
-      style={{   
+    <div
+      style={{
         backgroundImage: `url(${background})`,
-        height: '100%',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        flexGrow: 1 }}>
-        <Hero
-          size="fullheight"
+        height: "100%",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        flexGrow: 1,
+      }}
+    >
+      <Hero
+        size="fullheight"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Box
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
+            height: "300px",
+            width: "300px",
+            margin: "auto",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            boxShadow: "0px 0px 5px #2e2c29",
           }}
         >
-          <Box
+          <img
+            src={logo}
+            className="trajectorLogo"
+            alt="Trajector Logo"
             style={{
-              height: '300px',
-              width: '300px',
-              margin: 'auto',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              boxShadow: '0px 0px 5px #2e2c29',
+              marginBottom: 24,
+            }}
+          />
+          <Heading
+            style={{
+              textAlign: "center",
             }}
           >
-            <img
-              src={logo}
-              className='trajectorLogo'
-              alt='Trajector Logo'
+            Login
+          </Heading>
+          <Button.Group align="center">
+            <Button
               style={{
-                marginBottom: 24
-            }}/>
-              <Heading
-              style={{
-                textAlign: 'center'
-                }}>Login</Heading>
-              <Button.Group align="center">
-                <Button
-                  style={{
-                    backgroundColor: '#0078d4',
-                    color: 'white'
-                  }}
-                  onClick={handleLogin}
-                >
-                  Microsoft Azure
-                </Button>
-              </Button.Group>
-          </Box>
-        </Hero>
-      </div>
+                backgroundColor: "#0078d4",
+                color: "white",
+              }}
+              onClick={handleLogin}
+            >
+              Microsoft Azure
+            </Button>
+          </Button.Group>
+        </Box>
+      </Hero>
+    </div>
   );
 }
 
