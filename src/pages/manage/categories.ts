b04adf289063df6a7ce9666da94f788d45281200
Tom Anthony Aguila <tom31aguila@gmail.com>
2024-01-12 08:21:20 -0800

    [add] categories field in create embedding

--- src/pages/manage/categories.ts
+++ src/pages/manage/categories.ts
@@ -1,19 +1,18 @@
-
 export const categories = [
-    {
-        value: 1,
-        label: 'Hardware',
-    },
-    {
-        value: 2,
-        label: 'Software',
-    },
-    {
-        value: 3,
-        label: 'Biological',
-    },
-    {
-        value: 4,
-        label: 'Test',
-    },
-  ]
+  {
+    value: 1,
+    label: "Hardware",
+  },
+  {
+    value: 2,
+    label: "Software",
+  },
+  {
+    value: 3,
+    label: "Biological",
+  },
+  {
+    value: 4,
+    label: "Test",
+  },
+];
--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Hero, Box, Button, Container, Form, Heading, Notification, Section, Table, Tile } from 'react-bulma-components';
-import Chat from '../../components/chat';
-import { FooterSection } from '../../components/Footer';
-import { NavigationBar } from '../../components/NavigationBar';
-import image from '../../media/image.png';
-import { useNavigate } from 'react-router-dom';
-import { useIsAuthenticated, useMsal } from '@azure/msal-react';
-import { InteractionStatus } from '@azure/msal-browser';
-import AdminPage from '../../components/AdminPage';
-import Select from 'react-select';
-import { categories } from './categories';
+import React, { useEffect, useState } from "react";
+import {
+  Hero,
+  Box,
+  Button,
+  Container,
+  Form,
+  Heading,
+  Notification,
+  Section,
+  Table,
+  Tile,
+} from "react-bulma-components";
+import Chat from "../../components/chat";
+import { FooterSection } from "../../components/Footer";
+import { NavigationBar } from "../../components/NavigationBar";
+import image from "../../media/image.png";
+import { useNavigate } from "react-router-dom";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { InteractionStatus } from "@azure/msal-browser";
+import AdminPage from "../../components/AdminPage";
+import Select from "react-select";
+import { categories } from "./categories";
 
 interface Category {
   value: string;
@@ -17,20 +28,24 @@ interface Category {
 }
 
 interface TestInterface {
-  _id: string,
-  input: string,
-  title: string,
-  score: number,
-  solution?: string
+  _id: string;
+  input: string;
+  title: string;
+  score: number;
+  solution?: string;
 }
 
 interface SettingsInterface {
-  enableEmbedding: boolean,
-  minimumScore: number
+  enableEmbedding: boolean;
+  minimumScore: number;
+}
+interface options {
+  value: number;
+  label: string;
 }
 
 function Manage() {
-  const endPoint = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+  const endPoint = process.env.REACT_APP_API_URL || "http://localhost:8000";
   const [loadingEmbedding, setLoadingEmbedding] = useState(false);
   const [loadingTest, setLoadingTest] = useState(false);
   const [loadingSave, setLoadingSave] = useState(false);
@@ -38,25 +53,26 @@ function Manage() {
   const [documentKeyword, setDocumentKeyword] = useState("");
   const [document, setDocument] = useState("");
   const [testDocument, setTestDocument] = useState("");
-  const [embeddingNotification, setEmbeddingNotification] = useState<any>(<></>);
+  const [embeddingNotification, setEmbeddingNotification] = useState<any>(
+    <></>
+  );
   const [settingsData, setSettingsData] = useState<SettingsInterface>({
     enableEmbedding: false,
     minimumScore: 90,
   });
 
-  const [documentCategory, setDocumentCategory] = useState([]);
+  const [documentCategory, setDocumentCategory] = useState<Array<options>>([]);
 
   const [createEmbeddingSuccess, setCreateEmbeddingSuccess] = useState(false);
   const [testResults, setTestResults] = useState<Array<TestInterface>>([]);
 
-
   let navigate = useNavigate();
-  
+
   //collapsing RELATED TOPICS
   const isAuthenticated = useIsAuthenticated();
 
   const { instance, inProgress } = useMsal();
-  const account = localStorage.getItem("account") || "{}"
+  const account = localStorage.getItem("account") || "{}";
 
   // Ensure authentication before performing certain actions
   // useEffect(()=> {
@@ -85,26 +101,26 @@ function Manage() {
     fetch(`${endPoint}/createEmbedding`, {
       method: "post",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        title: documentTitle, 
-        keyword: document, 
+        title: documentTitle,
+        keyword: document,
         input: documentKeyword,
-        categories: (documentCategory as Category[]).map(category => category.value),
-      })
-    }).then(()=> {
-      setDocument("");
-      setEmbeddingNotification(
-        <Notification color="success">
-          Embedding Created
-      </Notification>
-      );
-      setCreateEmbeddingSuccess(true);
-    }).finally(()=> {
-      setLoadingEmbedding(false);
+        categories: documentCategory.map((option) => option.label),
+      }),
     })
-  }
+      .then(() => {
+        setDocument("");
+        setEmbeddingNotification(
+          <Notification color="success">Embedding Created</Notification>
+        );
+        setCreateEmbeddingSuccess(true);
+      })
+      .finally(() => {
+        setLoadingEmbedding(false);
+      });
+  };
 
   // Function to save settings
   const handleSaveSettings = () => {
@@ -112,43 +128,49 @@ function Manage() {
     fetch(`${endPoint}/saveSettings`, {
       method: "post",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         settingsData: settingsData,
-      })
-    }).then((res)=> {
-      return res.json()
-    }).then((res)=> {
-      setTestResults(res.related);
-    }).finally(()=> {
-      setLoadingSave(false);
+      }),
     })
-  }
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setTestResults(res.related);
+      })
+      .finally(() => {
+        setLoadingSave(false);
+      });
+  };
 
   // Function to fetch and update settings
   const getSettings = () => {
     setLoadingSave(true);
     fetch(`${endPoint}/getSettings`, {
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-    }).then((res)=> {
-      return res.json()
-    }).then((res)=> {
-      setSettingsData(res);
-    }).finally(()=> {
-      setLoadingSave(false);
     })
-  }
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setSettingsData(res);
+      })
+      .finally(() => {
+        setLoadingSave(false);
+      });
+  };
 
   // Function to update settings fields
   const updateSettingsFields = (field: string, value: any) => {
     setSettingsData({
       ...settingsData,
       [field]: value,
-    })
-  }
+    });
+  };
 
   // Function to test embeddings
   const handleTestEmbeddings = () => {
@@ -156,115 +178,120 @@ function Manage() {
     fetch(`${endPoint}/testEmbedding`, {
       method: "post",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         keyword: testDocument,
-      })
-    }).then((res)=> {
-      return res.json()
-    }).then((res)=> {
-      setTestResults(res.related);
-    }).finally(()=> {
-      setLoadingTest(false);
+      }),
     })
-  }
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setTestResults(res.related);
+      })
+      .finally(() => {
+        setLoadingTest(false);
+      });
+  };
 
   // Fetch and update settings on component mount
-  useEffect(()=> {
+  useEffect(() => {
     getSettings();
-  }, [])
+  }, []);
 
   return (
-
-    <Hero 
-    hasNavbar={true}
-    size="fullheight"
+    <Hero
+      hasNavbar={true}
+      size="fullheight"
       style={{
-        backgroundColor:'#A7C6ED',
+        backgroundColor: "#A7C6ED",
         backgroundImage: `url(${image})`,
-        height: '100vh',
-        backgroundSize: 'cover',
+        height: "100vh",
+        backgroundSize: "cover",
       }}
     >
-      <NavigationBar/>
+      <NavigationBar />
       <AdminPage>
-        <Box style={{margin: 20, marginTop: '100px', padding: 0, paddingTop: '20px'}}>
-          <Section style={{paddingTop: 0, paddingBottom: 0, marginBottom: 15}}>
-            <Heading size={3}>
-              Embeddings
-            </Heading>
-            <Box shadowless style={{paddingTop: 0}}>
-              <Section size="small" style={{paddingTop:0, paddingBottom: 0}}>
-                <Heading size={4}>
-                  Create Embedding
-                </Heading>
+        <Box
+          style={{
+            margin: 20,
+            marginTop: "100px",
+            padding: 0,
+            paddingTop: "20px",
+          }}
+        >
+          <Section
+            style={{ paddingTop: 0, paddingBottom: 0, marginBottom: 15 }}
+          >
+            <Heading size={3}>Embeddings</Heading>
+            <Box shadowless style={{ paddingTop: 0 }}>
+              <Section size="small" style={{ paddingTop: 0, paddingBottom: 0 }}>
+                <Heading size={4}>Create Embedding</Heading>
                 {embeddingNotification}
                 <Form.Field>
                   <Form.Control>
                     {/* Topic Area */}
-                    <Form.Label>
-                      Title
-                    </Form.Label>
+                    <Form.Label>Title</Form.Label>
                     <Form.Control>
                       <Form.Input
-                        onChange={(e)=>setDocumentTitle(e.target.value)}
+                        onChange={(e) => setDocumentTitle(e.target.value)}
                         placeholder="Enter Title"
                         type="text"
                       />
                     </Form.Control>
 
                     {/* keywords Area */}
-                    <Form.Label>
-                      Keywords
-                    </Form.Label>
+                    <Form.Label>Keywords</Form.Label>
                     <Form.Control>
                       <Form.Input
-                        onChange={(e)=>setDocumentKeyword(e.target.value)}
+                        onChange={(e) => setDocumentKeyword(e.target.value)}
                         placeholder="e.g. mouse problem"
                         type="text"
                       />
                     </Form.Control>
 
                     {/* Categories Area */}
-                  <Form.Label>
-                    Categories
-                  </Form.Label>
-                      <Select
-                        isMulti
-                        name="colors"
-                        options={categories}
-                        // value={documentCategory}
-                        onChange={(selectedOptions: any) => setDocumentCategory(selectedOptions)}
-                        placeholder="Select a category"
-                      />
+                    <Form.Label>Categories</Form.Label>
+                    <Select
+                      isMulti
+                      name="colors"
+                      options={categories}
+                      // value={documentCategory}
+                      onChange={(selectedOptions: any) =>
+                        setDocumentCategory(selectedOptions)
+                      }
+                      placeholder="Select a category"
+                    />
 
                     {/* description area */}
-                    <Form.Label>
-                      Description / Solution
-                    </Form.Label>
+                    <Form.Label>Description / Solution</Form.Label>
                     <Form.Textarea
-                      id='textareaDescription'
-                      onChange={(e)=>setDocument(e.target.value)}
+                      id="textareaDescription"
+                      onChange={(e) => setDocument(e.target.value)}
                       value={document}
-                      
                     />
                   </Form.Control>
                 </Form.Field>
                 <Form.Field kind="group">
                   <Form.Control>
-                    <Button style={{boxShadow: '0px 0px 5px #888888'}} color="link" onClick={handleCreateEmbeddings} loading={loadingEmbedding}>Create Embedding</Button>
+                    <Button
+                      style={{ boxShadow: "0px 0px 5px #888888" }}
+                      color="link"
+                      onClick={handleCreateEmbeddings}
+                      loading={loadingEmbedding}
+                    >
+                      Create Embedding
+                    </Button>
                   </Form.Control>
                 </Form.Field>
               </Section>
-              <Section size="small" style={{paddingBottom: 0}}>
-                <Heading size={4}>
-                  Test Embedding
-                </Heading>
+              <Section size="small" style={{ paddingBottom: 0 }}>
+                <Heading size={4}>Test Embedding</Heading>
                 <Form.Field>
                   <Form.Control>
                     <Form.Textarea
-                      onChange={(e)=>setTestDocument(e.target.value)}
+                      onChange={(e) => setTestDocument(e.target.value)}
                       // value={testDocument}
                       placeholder="Search"
                     />
@@ -272,7 +299,8 @@ function Manage() {
                 </Form.Field>
                 <Form.Field kind="group">
                   <Form.Control>
-                    <Button style={{boxShadow: '0px 0px 5px #888888'}}
+                    <Button
+                      style={{ boxShadow: "0px 0px 5px #888888" }}
                       color="link"
                       onClick={handleTestEmbeddings}
                       loading={loadingTest}
@@ -281,55 +309,43 @@ function Manage() {
                     </Button>
                   </Form.Control>
                 </Form.Field>
-                {(testResults && testResults.length > 0) && (
+                {testResults && testResults.length > 0 && (
                   <Table.Container>
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th>
-                          Document
-                        </th>
-                        <th>
-                          Solution
-                        </th>
-                        <th>
-                          Score
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {testResults.map((test)=> {
-                        return (
-                          <tr>
-                            <td>
-                              {test.input.substring(0, 100)}...
-                            </td>
-                            <td>
-                              {test.solution}
-                            </td>
-                            <td>
-                              {test.score}
-                            </td>
-                          </tr>
+                    <Table>
+                      <thead>
+                        <tr>
+                          <th>Document</th>
+                          <th>Solution</th>
+                          <th>Score</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {testResults.map((test) => {
+                          return (
+                            <tr>
+                              <td>{test.input.substring(0, 100)}...</td>
+                              <td>{test.solution}</td>
+                              <td>{test.score}</td>
+                            </tr>
                           );
-                      })}
-                    </tbody>
-                  </Table>
+                        })}
+                      </tbody>
+                    </Table>
                   </Table.Container>
                 )}
               </Section>
             </Box>
           </Section>
-          <Section style={{paddingTop: 0, paddingBottom: 0}}>
-            <Heading size={3}>
-              Settings
-            </Heading>
-            <Box shadowless> 
+          <Section style={{ paddingTop: 0, paddingBottom: 0 }}>
+            <Heading size={3}>Settings</Heading>
+            <Box shadowless>
               <Form.Field>
                 <Form.Control>
-                  <Form.Checkbox 
+                  <Form.Checkbox
                     checked={settingsData.enableEmbedding}
-                    onChange={(e)=>updateSettingsFields('enableEmbedding',e.target.checked)} 
+                    onChange={(e) =>
+                      updateSettingsFields("enableEmbedding", e.target.checked)
+                    }
                   >
                     Enable Embeddings
                   </Form.Checkbox>
@@ -338,17 +354,19 @@ function Manage() {
               <Form.Field>
                 <Form.Label>Minimum Score</Form.Label>
                 <Form.Control>
-                  <Form.Input 
-                    type="number" 
-                    onChange={(e)=>updateSettingsFields('minimumScore',e.target.value)} 
-                    placeholder="90" 
+                  <Form.Input
+                    type="number"
+                    onChange={(e) =>
+                      updateSettingsFields("minimumScore", e.target.value)
+                    }
+                    placeholder="90"
                     value={settingsData.minimumScore}
                   />
                 </Form.Control>
               </Form.Field>
               <Form.Field kind="group">
                 <Form.Control>
-                  <Button 
+                  <Button
                     color="link"
                     onClick={handleSaveSettings}
                     loading={loadingSave}
@@ -361,7 +379,7 @@ function Manage() {
           </Section>
         </Box>
       </AdminPage>
-      <FooterSection/>
+      <FooterSection />
     </Hero>
   );
 }
